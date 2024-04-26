783690f056ee3332af9ea09511b2a6b92f4d6f66
Basliel-Amsalu <acychappiekiller@gmail.com>
2024-04-26 09:46:16 +0300

    moved db into a separate folder and created a person schema

---
+++ db/db.js
@@ -0,0 +1,10 @@
+let persons = [
+  {
+    id: "1",
+    name: "Sam",
+    age: "26",
+    hobbies: [],
+  },
+]; //This is your in memory database
+
+module.exports = persons;
--- index.js
+++ index.js
@@ -1,17 +1,11 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
-let persons = [{
-    id: '1',
-    name: 'Sam',
-    age: '26',
-    hobbies: []    
-}] //This is your in memory database
 
-app.set('db', persons)
+app.set("db", persons);
 //TODO: Implement crud of person
 
 if (require.main === module) {
-    app.listen(3000)
+  app.listen(3000);
 }
 module.exports = app;
